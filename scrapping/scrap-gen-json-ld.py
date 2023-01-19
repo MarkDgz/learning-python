9283188d252611bdaf36f9d969e320a5da5a4cbe
Mark Dgz <tnemexico@gmail.com>
2023-01-19 01:30:07 -0300

    Adding parameters to send URL
    Adding parameters to send URL

--- scrapping/scrap-gen-json-ld.py
+++ scrapping/scrap-gen-json-ld.py
@@ -1,9 +1,28 @@
+# Program:     scrap-gen-json-ld.py
+# Description: Exercise about scrap data and generating information into JSON-LD format
+# Changes: 
+# 1. New Program
+# Licence: MIT
+# Dev status: Latest Stable Version 1.0.0
+
+# # Required libraries
+# python programs to support file and directory functions
+import os
+import sys
+
+#Parameters
+if len(sys.argv):
+    text = sys.argv[1]
+else:
+    print("Put the URL Text as Paremeter, poner la URL o Web Link como paramentro")
+
 #Exercise about scrap data and generating information into JSON-LD format
 import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://www.example.com"
+#url = "https://www.example.com"
+url = text
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
